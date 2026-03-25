9886956d3095642ed7b2234191582c24727784bf
shaoli-lu <shaoli_lu@yahoo.com>
2026-03-24 20:30:11 -0500

    more 7

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,6 +1,6 @@
 export const SYMBOLS = [
   "NVDA", "TSM", "AVGO", "AMD", "INTC", "AMZN", "MSFT", "GOOGL", "AAPL",
-  "BKNG", "SAP", "QCOM", "CSCO", "NOW", "PLTR", "FIG", "TSLA", "META"
+  "BKNG", "SAP", "QCOM", "CSCO", "NOW", "PLTR", "FIG", "TSLA", "META", "COST", "ORCL", "DELL", "CRM", "UBER", "WDAY", "GILD"
 
 ];
 
